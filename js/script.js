14fde2ee4edcc2a5fd41665f998bdb128e34863d
lipetici <fellipeandre160@gmail.com>
2025-02-01 15:03:51 -0300

    15555555555555

--- js/script.js
+++ js/script.js
@@ -13,17 +13,3 @@ document.getElementById('flight-search-form').addEventListener('submit', functio
     // Redirecionar para a página de resultados com os parâmetros de pesquisa
     window.location.href = `search-results.html?origin=${origin}&destination=${destination}&departureDate=${departureDate}&returnDate=${returnDate}`;
 });
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let errorMessage = document.getElementById("error-message");
-
-    if (email === "admin@teste.com" && password === "1234") {
-        alert("Login bem-sucedido!");
-        window.location.href = "dashboard.html"; // Redireciona para outra página
-    } else {
-        errorMessage.textContent = "E-mail ou senha incorretos.";
-    }
-});
